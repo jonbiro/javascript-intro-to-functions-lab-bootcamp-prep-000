18d79de7d9e8bb1bf8040b257e0db8aee5905c47
Jonathan Biro <jonnybrx@gmail.com>
2018-10-15 01:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
   return "I can't hear you!";
-  } else if ()
-}
-
-function sayHiToGrandma(string){
-  if (string === string.toUpperCase()){
+  } else if (string === string.toUpperCase()){
   return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma."){
+  } else if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
+
+// function sayHiToGrandma(string){
+//   if (string === string.toUpperCase()){
+//   return "YES INDEED!";
+//   }
+// }
+
+// function sayHiToGrandma(string){
+//   if (string === "I love you, Grandma."){
+//     return "I love you, too.";
+//   }
+// }
